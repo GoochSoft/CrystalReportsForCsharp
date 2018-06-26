2dbd4dd4eeab6b311bd281e35665bca3ff10b359
Brandon <goochsoft@gmail.com>
2018-06-26 12:53:43 -0500

    Add CrystalDecisions refs

---
+++ src/CrystalReportsForCsharp/Export.cs
@@ -0,0 +1,22 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System.Data;
+
+namespace CrystalReportsForCsharp
+{
+    public class Export
+    {
+        public void Pdf()
+        {
+            var data = new DataTable();
+            data.Columns.Add("ProductId", typeof(int));
+            data.Columns.Add("Name", typeof(string));
+            data.Columns.Add("Description", typeof(string));
+
+            data.Rows.Add(1, "Panda Coffee Mug", "Ceramic, black & white pandabear coffee mug");
+            data.Rows.Add(1, "Coffee Stir Sticks", "Disposable coffee stir sticks");
+
+            var report = new ReportDocument();
+
+        }
+    }
+}
